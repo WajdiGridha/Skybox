cbd120e963a01836739846449dccecaaec996404
wajdi gr <wajdigridha744@gmail.com>
2022-01-26 20:00:15 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import { Canvas, extend, useThree, useFrame } from "react-three-fiber";
+import {
+  CubeTextureLoader,
+  CubeCamera,
+  WebGLCubeRenderTarget,
+  RGBFormat,
+  LinearMipmapLinearFilter
+} from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import "./styles.css";
 
+extend({ OrbitControls });
+
+const CameraControls = () => {
+  const {
+    camera,
+    gl: { domElement }
+  } = useThree();
+
+  const controls = useRef();
+  useFrame(() => controls.current.update());
+  return (
+    <orbitControls
+      ref={controls}
+      args={[camera, domElement]}
+      autoRotate={true}
+      enableZoom={false}
+    />
+  );
+};
+
+function SkyBox() {
+  const { scene } = useThree();
+  const loader = new CubeTextureLoader();
+  const texture = loader.load([
+    "/1.jpg",
+    "/2.jpg",
+    "/3.jpg",
+    "/4.jpg",
+    "/5.jpg",
+    "/6.jpg"
+  ]);
+
+  scene.background = texture;
+  
+  return null;
+}
+
+function Sphere() {
+  const { scene, gl } = useThree();
+  const cubeRenderTarget = new WebGLCubeRenderTarget(256, {
+    format: RGBFormat,
+    generateMipmaps: true,
+    minFilter: LinearMipmapLinearFilter
+  });
+  const cubeCamera = new CubeCamera(1, 1000, cubeRenderTarget);
+  cubeCamera.position.set(0, 0, 0);
+  scene.add(cubeCamera);
+
+  useFrame(() => cubeCamera.update(gl, scene));
+
+  return (
+    <mesh visible position={[0, 0, 0]} rotation={[0, 0, 0]} castShadow>
+      <directionalLight intensity={0.5} />
+      <sphereGeometry attach="geometry" args={[2, 32, 32]} />
+      <meshBasicMaterial
+        attach="material"
+        envMap={cubeCamera.renderTarget.texture}
+        color="white"
+        roughness={0.1}
+        metalness={1}
+      />
+    </mesh>
+  );
+}
+
+// Lights
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas className="canvas">
+      <CameraControls />
+      <Sphere />
+      <SkyBox />
+    </Canvas>
   );
+  
 }
 
 export default App;
